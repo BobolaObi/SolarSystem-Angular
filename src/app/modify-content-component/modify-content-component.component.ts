1f32ceac6122d24079e74d2d4f5d8d5fe71c8bbf
BOBOLA <w0772193@myscc.ca>
2022-04-03 23:43:38 -0400

    Added card Component

---
+++ src/app/modify-content-component/modify-content-component.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Content} from "../helper-files/content-interface";
+import {PlanetServiceService} from "../Services/planet-service.service";
+
+@Component({
+  selector: 'app-modify-content-component',
+  templateUrl: './modify-content-component.component.html',
+  styleUrls: ['./modify-content-component.component.scss']
+})
+export class ModifyContentComponentComponent implements OnInit {
+  newContent: Content;
+  temptags: string = "";
+  tempid: string = "";
+  @Output() newContentEvent: EventEmitter<Content> = new EventEmitter<Content>();
+  @Output() updateContentEvent: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private planetService: PlanetServiceService) {
+    this.newContent = { title: '', description: '', creator: '' };
+  }
+
+  ngOnInit(): void {
+  }
+
+addItem(): void {
+    if (this.tempid !== "") {
+      this.newContent.tags = this.temptags ? this.temptags.split(",") : [];
+      this.newContent.id = parseInt(this.tempid);
+
+      this.planetService.updateContent(this.newContent).subscribe(() => {
+        this.updateContentEvent.emit();
+        this.newContent = { title: '', description: '', creator: '', imgURL: "", type: "", tags: [] };
+        this.temptags = "";
+        this.tempid = "";
+      });
+    }
+    else {
+      this.newContent.tags = this.temptags ? this.temptags.split(",") : [];
+
+      this.planetService.addContent(this.newContent).subscribe(newContentFromServer => {
+        this.newContentEvent.emit(newContentFromServer);
+        this.newContent = { title: '', description: '', creator: '', imgURL: "", type: "", tags: [] };
+        this.temptags = "";
+      });
+    }
+  }
+}
