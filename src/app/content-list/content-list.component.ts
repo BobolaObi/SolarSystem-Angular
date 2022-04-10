12995eef7406c33aa8cae86a9fec1359566da5c1
BOBOLA <w0772193@myscc.ca>
2022-04-10 09:18:47 -0400

    modified and moved Content list from app component

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Content} from "../helper-files/content-interface";
 import {PlanetServiceService} from "../Services/planet-service.service";
+import {MessageServiceService} from "../Services/message-service.service";
+
+
 
 // imgURL: "https://scitechdaily.com/images/Magellan-Venus-Surface.jpg", --Content for firs planet
 @Component({
@@ -8,17 +11,24 @@ import {PlanetServiceService} from "../Services/planet-service.service";
   templateUrl: "./content-list.component.html",
   styleUrls: ['./content-list.component.scss']
 })
+
+
 export class ContentListComponent implements OnInit {
   planets: Content[];
   searchTitle?: boolean;
+  title = 'Assignment1';
 
-  constructor(private planetsService: PlanetServiceService) {
+  chosenPlanet?: Content;
+
+  constructor(private planetServiceService: PlanetServiceService,
+              private messageService: MessageServiceService) {
+
     this.planets = [];
   }
 
-  ngOnInit(): void {
-    this.planetsService.getContent().subscribe(listedPlanets => this.planets = listedPlanets);
-  }
+  // ngOnInit(): void {
+  //   this.planetsService.getContent().subscribe(listedPlanets => this.planets = listedPlanets);
+  // }
 
   //The type we are searching for is a string
   checkForTitle(title: string): void {
@@ -40,5 +50,30 @@ export class ContentListComponent implements OnInit {
     this.planets.push(newContent);
     this.planets = [...this.planets];
     console.log("my list after cloning: ", this.planets);
+  }
+
+  //content from app component
+  ngOnInit(): void{
+    // this.planetsService.getContent().subscribe(listedPlanets => this.planets = listedPlanets);
+
+    this.planetServiceService.getContentItem(5).subscribe(
+      planetAtIndex => this.chosenPlanet = planetAtIndex
+    );
+  }
+  displayItem(id: string): void{
+    if (!parseInt(id)) {
+      this.messageService.add("Please enter a number value");
+      return;
+    }
+    let idNumber = parseInt(id);
+    this.planetServiceService.getContent().subscribe(arrayOfPlanets => {
+      let planetInArray = arrayOfPlanets.find(chosenPlanet => chosenPlanet.id === idNumber);
+      if (!planetInArray) {
+        this.messageService.add("Please enter a number value for a valid id");
+      }
+      else {
+        this.chosenPlanet = planetInArray;
+      }
+    });
   }
 }
