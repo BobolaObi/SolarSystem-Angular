523250dd3990c01e4246977b0d9d8dad13086e51
BOBOLA <w0772193@myscc.ca>
2022-04-21 11:15:32 -0400

    Update Content fucntionalityy added

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Content} from "../helper-files/content-interface";
 import {PlanetServiceService} from "../Services/planet-service.service";
+import {MessageServiceService} from "../Services/message-service.service";
 
 // imgURL: "https://scitechdaily.com/images/Magellan-Venus-Surface.jpg", --Content for firs planet
 @Component({
@@ -12,12 +13,14 @@ export class ContentListComponent implements OnInit {
   planets: Content[];
   searchTitle?: boolean;
 
-  constructor(private planetsService: PlanetServiceService) {
+  constructor(private planetsService: PlanetServiceService, private messageService : MessageServiceService) {
     this.planets = [];
   }
 
   ngOnInit(): void {
-    this.planetsService.getContent().subscribe(listedPlanets => this.planets = listedPlanets);
+    this.planetsService.getContent().subscribe(listedPlanets =>
+      this.planets = listedPlanets);
+    this.messageService.add("List added");
   }
 
   //The type we are searching for is a string
@@ -35,6 +38,12 @@ export class ContentListComponent implements OnInit {
 
   }
 
+  updateContentOnList(): void{
+    this.planetsService.getContent().subscribe(listOfDigimon => {
+      this.planets = listOfDigimon;
+      this.messageService.add("Added item Sucessfully");
+    });
+  }
   addContentToList(newContent: Content): void {
     console.log("content that came from the child element: ", newContent);
     this.planets.push(newContent);
